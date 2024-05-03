9fcf7c8e437fa949b69dd9f87e7e9397e7a8cd8e
KotovaSophia <145443920+KotovaSophia@users.noreply.github.com>
2024-05-03 22:14:56 +0300

    Add files via upload

---
+++ Program-10.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите число x: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите число n: ");
+        double n = Convert.ToDouble(Console.ReadLine());
+        double res = 0;
+
+        if (Math.Abs(x) <= 1)
+        {
+            for (double i = 0; i <= n; i++)
+            {
+                res += ((Math.Pow(-1, n + 1)) * (Math.Pow(x, (2 * n + 1)))) / (2 * n + 1);
+            }
+            Console.WriteLine(res);
+        }
+        else
+        {
+            Console.WriteLine("Модуль x > 1");
+        }
+    }
+}
